872f05fec6bf430a933baeb462443a364c77e783
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2020-12-14 22:59:15 +0530

    int

---
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/lastpage.java
@@ -0,0 +1,14 @@
+package com.example.mad_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lastpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lastpage);
+    }
+}
