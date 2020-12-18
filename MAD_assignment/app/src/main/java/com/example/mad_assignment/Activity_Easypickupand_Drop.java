8480234fca4fe70edfb90fc73fadbab67c85b037
DamiduN <damindu.nehan@gmail.com>
2020-12-18 21:47:02 +0530

    changed

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Easypickupand_Drop.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Easypickupand_Drop.java
@@ -2,5 +2,5 @@ package com.example.mad_assignment;
 
 import android.app.Activity;
 
-public class Activity_easypickupand_drop extends Activity {
+public class Activity_Easypickupand_Drop extends Activity {
 }
