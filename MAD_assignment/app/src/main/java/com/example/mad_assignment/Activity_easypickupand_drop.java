92ba05a03aba5ea62b4fc03bd0fe7345b31561c3
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2020-12-18 21:44:24 +0530

    mad

---
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_easypickupand_drop.java
@@ -0,0 +1,6 @@
+package com.example.mad_assignment;
+
+import android.app.Activity;
+
+public class Activity_easypickupand_drop extends Activity {
+}
--- MAD_assignment/app/src/main/java/com/example/mad_assignment/easypickupand_drop.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/easypickupand_drop.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_Easypickup extends AppCompatActivity {
+public class easypickupand_drop extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_easypickup);
+        setContentView(R.layout.activity_easypickupand_drop);
     }
 }
