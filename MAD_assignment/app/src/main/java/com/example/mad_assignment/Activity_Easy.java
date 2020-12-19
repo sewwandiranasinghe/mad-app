cb256b16e8bc8f919d9bb78301c92fdcc749743a
DamiduN <damindu.nehan@gmail.com>
2020-12-19 21:16:41 +0530

    codes for buttons in easy pickup

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Easy.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Easy.java
@@ -2,7 +2,9 @@ package com.example.mad_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Easy extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Activity_Easy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__easy);
+    }
+
+    public void bookservicebutton(View view) {
+        Intent intent = new Intent(Activity_Easy.this, Activity_Services.class);
+        startActivity(intent);
+    }
+
+    public void freepickupbutton(View view) {
+        Intent intent = new Intent(Activity_Easy.this, Activity_Login.class);
+        startActivity(intent);
+    }
+
+    public void billservicebutton(View view) {
+        Intent intent = new Intent(Activity_Easy.this, Activity_Billingdetails.class);
+        startActivity(intent);
     }
 }
