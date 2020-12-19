eb46a4236f6875d5b930c3e3f44fe32aa68ecd13
DamiduN <damindu.nehan@gmail.com>
2020-12-19 14:11:23 +0530

    back

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Signup.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Signup.java
@@ -3,12 +3,19 @@ package com.example.mad_assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Activity_Signup extends AppCompatActivity {
+
+    private EditText fullnametx,addresstx,mobilenotx,passtx;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
     }
 }
