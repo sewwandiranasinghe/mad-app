f340b6f2ba091657f96f930a8e019c9d3ccc11a3
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2020-12-22 15:35:22 +0530

    newnew

---
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Licencereview.java
@@ -0,0 +1,14 @@
+package com.example.mad_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Licencereview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__licencereview);
+    }
+}
