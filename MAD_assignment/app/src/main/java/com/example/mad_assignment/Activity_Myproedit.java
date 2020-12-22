a5746004166a8c3106067e82e461f552ab70e295
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2020-12-22 10:44:39 +0530

    new mad

---
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Myproedit.java
@@ -0,0 +1,14 @@
+package com.example.mad_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Myproedit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__myproedit);
+    }
+}
