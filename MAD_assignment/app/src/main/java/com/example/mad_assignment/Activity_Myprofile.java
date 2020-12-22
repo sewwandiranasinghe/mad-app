dbaf31864af2fbaa04f8cd3688a6b985cbf58027
DamiduN <damindu.nehan@gmail.com>
2020-12-22 19:42:17 +0530

    connected view button in the myprofile

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Myprofile.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Myprofile.java
@@ -2,7 +2,9 @@ package com.example.mad_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Myprofile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Activity_Myprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+    }
+
+    public void viewbutton(View view) {
+        Intent intent = new Intent(Activity_Myprofile.this, Activity_Licencereview.class);
+        startActivity(intent);
     }
 }
