12f6c5ccd042279b3866b301f92b6b9b964cfe74
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2020-12-28 00:55:19 +0530

    newmad

---
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Forgotpass.java
@@ -0,0 +1,14 @@
+package com.example.mad_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Forgotpass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__forgotpass);
+    }
+}
