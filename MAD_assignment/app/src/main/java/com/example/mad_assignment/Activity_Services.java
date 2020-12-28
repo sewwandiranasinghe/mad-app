a635efd2a989b13a3a065cee4dcec9534d5931ed
DamiduN <damindu.nehan@gmail.com>
2020-12-28 18:33:21 +0530

    mmm

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Services.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Services.java
@@ -2,7 +2,9 @@ package com.example.mad_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Services extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Activity_Services extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+    }
+
+    public void bookbutton(View view) {
+        Intent intent = new Intent(Activity_Services.this, Activity_Easy.class);
+        startActivity(intent);
     }
 }
