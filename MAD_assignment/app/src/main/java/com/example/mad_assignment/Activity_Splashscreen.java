b08a2ac76d722c380e5c7c77c3cd211a3865c55f
DamiduN <damindu.nehan@gmail.com>
2021-01-01 22:04:48 +0530

    ggg

--- MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Splashscreen.java
+++ MAD_assignment/app/src/main/java/com/example/mad_assignment/Activity_Splashscreen.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Activity_Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splashscreen);
+        getSupportActionBar().hide();
+
         Thread thread = new Thread() {
 
             @Override
